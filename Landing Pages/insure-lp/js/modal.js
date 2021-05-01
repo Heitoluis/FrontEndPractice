3ec4e0a792ee70b37b80c6a69687199deab96644
Héctor Vásquez <hvasquez@T-IDSOLUTIONS.COM>
2021-05-01 11:50:07 -0500

    overflow body y hidden

--- Landing Pages/insure-lp/js/modal.js
+++ Landing Pages/insure-lp/js/modal.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector(".icon-menu");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".icon-close");
+const body = document.querySelector("body");
 
 menu.addEventListener("click", () => {
   modal.classList.toggle("modal-display");
+  body.style.overflowY = "hidden";
 });
 
 close.addEventListener("click", () => {
   modal.classList.toggle("modal-display");
+  body.style.overflowY = "scroll";
 });
